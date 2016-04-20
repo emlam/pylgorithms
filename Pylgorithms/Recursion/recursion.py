b32cc06591f84bab35ec623b31e99212bb1f5b13
j3dev <j3pydev@gmail.com>
2016-04-19 21:58:57 -0700

    added recursion

--- Pylgorithms/Recursion/recursion.py
+++ Pylgorithms/Recursion/recursion.py
@@ -2,6 +2,35 @@
 # Chapter 2 - Recursion and Backtracking
 # "Data Structures and Algorithmic Thinking
 # with Python" by Narasimha Karumanchi
-# Amazon: http://www.amazon.com/dp/8192107590/ref=cm_sw_r_tw_dp_Zlafxb17MAD8M
+# see readme
 ################################################
 
+
+# Tower of Hanoi p.44
+def towersHanoi(numDisks, startPeg=1, endPeg=3):
+    if numDisks:
+        towersHanoi(numDisks-1, startPeg, 6-startPeg-endPeg)
+        print "Move disk %d from peg %d to peg %d" % (numDisks, startPeg, endPeg)
+        towersHanoi(numDisks-1, 6-startPeg-endPeg, endPeg)
+        
+        
+# Check if array is sorted p.45
+def isSorted(array):
+    # base case
+    if len(array) == 1:
+        return True
+    return array[0] <= array[1] and isSorted(array[1:])
+
+
+# Sorted check modified to return false if not sorted
+def isSorted(array):
+    if len(array) == 1:
+        return True
+    elif array[0] <= array[1]:
+        return isSorted(array[1:])
+    else:
+        return False
+
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n) for recursive stack space
--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 ##The unofficial Python 2.7 package of "Data Structures and Algorithmic Thinking in Python" by Narasimha Karumanchi
 
+##Amazon: ['Data Structures and Algorithmic Thinking in Python' by Narasimha Karumanchi][1]
+
+##Github: ['Data Structures and Algorithmic Thinking in Python' on Github][2]
+
 ###This software is provided "as is" without any warranty; without even the implied warranty of merchantability or fitness for a particular purpose. 
 
-###I do not recommending using these algorithms or this package.  This is for educational purposes.
+###I do not recommend using these algorithms or this package.  This is for educational purposes.
 
 ##Current Status:
 ##Incomplete (barely started - even the setup for this package isn't done) v1.0.0.dev1
+
+[1]: http://www.amazon.com/dp/8192107590/ref=cm_sw_r_tw_dp_Zlafxb17MAD8M  "'Data Structures and Algorithmic Thinking in Python' by Narasimha Karumanchi"
+[2]: https://github.com/careermonk/DataStructureAndAlgorithmicThinkingWithPython  "'Data Structures and Algorithmic Thinking in Python' on Github"
