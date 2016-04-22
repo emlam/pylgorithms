5ea37c8d6bb960fa2eca4094cdfbc26ed472b39c
Emily Lam <mail@emilymlam.com>
2016-04-21 20:35:17 -0700

    node class

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,8 @@
 """Linked List"""
+
+#Start with a Node for our Linked List.
+class Node:
+    #our constructor for when we call our Node class.
+    def __init__(self):
+        self.data = None
+        self.next = None
